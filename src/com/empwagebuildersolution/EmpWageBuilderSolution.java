7cdc82f77a703b80528fc1d1b477c11bd0224ed8
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-25 15:15:38 +0530

    Attendence checking

--- src/com/empwagebuildersolution/EmpWageBuilderSolution.java
+++ src/com/empwagebuildersolution/EmpWageBuilderSolution.java
@@ -3,7 +3,14 @@ package com.empwagebuildersolution;
 public class EmpWageBuilderSolution {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to empwage calculation");
+		System.out.println("Welcome to empwage");
+		int IS_EMP_PRESENT = 1;
+		double empCheck = Math.floor(Math.random() * 10 %2 );
+		if(empCheck == IS_EMP_PRESENT)	
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+
 
 	}
 
