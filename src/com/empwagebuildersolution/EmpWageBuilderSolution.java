37a61b229c26f0ce904342e2a3d6f4a123027e9e
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-25 18:19:00 +0530

    empwage calculation using switch -case

--- src/com/empwagebuildersolution/EmpWageBuilderSolution.java
+++ src/com/empwagebuildersolution/EmpWageBuilderSolution.java
@@ -1,34 +1,28 @@
 package com.empwagebuildersolution;
 
 public class EmpWageBuilderSolution {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
-		//Constatnts
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int EMP_RATE_PER_HOUR =20;
 		//Variables
-		int empHrs =0;
-		int empWage =0;
+		int empHrs = 0;
+		int empWage = 0;
 		//Computation
-		double empCheck = Math.floor(Math.random() * 10 ) % 3 ;
-		if(empCheck == IS_FULL_TIME) {
-			empHrs = 8;
-			empWage = empHrs *  EMP_RATE_PER_HOUR;
-			System.out.println(" Full Time Emp WAGE " +empWage );
+		int empCheck =(int) Math.floor(Math.random() * 10 ) % 3 ;
+		switch (empCheck){
+		case  IS_PART_TIME:
+			empHrs =4;
+			break;
+		case IS_FULL_TIME:
+			empHrs =8;
+			break;
+		default:
+			empHrs =0;
 		}
-		else if(empCheck == IS_PART_TIME) {
-			empHrs = 4;
-			empWage = empHrs *  EMP_RATE_PER_HOUR;
-			System.out.println("Part TimeEmp WAGE " +empWage );
-		}
-		else { 
-			empHrs= 0;
-			empWage = empHrs *  EMP_RATE_PER_HOUR;
-			System.out.println("Absent Emp WAGE " +empWage );
-		}
-
-
+		empWage = empHrs *  EMP_RATE_PER_HOUR;
+		System.out.println("Emp WAGE " +empWage );
 
 	}
 
