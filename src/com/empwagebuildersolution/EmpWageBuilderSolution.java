3218957a117c228583b280b19b7ff5f33c2e709e
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-25 18:40:17 +0530

    month empwage

--- src/com/empwagebuildersolution/EmpWageBuilderSolution.java
+++ src/com/empwagebuildersolution/EmpWageBuilderSolution.java
@@ -4,26 +4,29 @@ public class EmpWageBuilderSolution {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-
+	public static final int NUM_WORKING_DAYS=20;
 	public static void main(String[] args) {
 		//Variables
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		//Computation
-		int empCheck =(int) Math.floor(Math.random() * 10 ) % 3 ;
-		switch (empCheck){
-		case  IS_PART_TIME:
-			empHrs =4;
-			break;
-		case IS_FULL_TIME:
-			empHrs =8;
-			break;
-		default:
-			empHrs =0;
+		for(int day = 0;day<=NUM_WORKING_DAYS;day++){
+			int empCheck =(int) Math.floor(Math.random() * 10 ) % 3 ;
+			switch (empCheck) {
+			case  IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}		
+			empWage = empHrs *  EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage ;
+			System.out.println("Emp WAGE: " + empWage );
 		}
-		empWage = empHrs *  EMP_RATE_PER_HOUR;
-		System.out.println("Emp WAGE " +empWage );
-
+		System.out.println("Month  EmpWage "+totalEmpWage);
 	}
-
 }
