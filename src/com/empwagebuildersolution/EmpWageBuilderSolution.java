4323810008fa1b9f584d72b0d62e028b0c9087ff
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-25 15:24:49 +0530

    full time employee salary

--- src/com/empwagebuildersolution/EmpWageBuilderSolution.java
+++ src/com/empwagebuildersolution/EmpWageBuilderSolution.java
@@ -4,12 +4,16 @@ public class EmpWageBuilderSolution {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to empwage");
-		int IS_EMP_PRESENT = 1;
+		int IS_EMP_PRESENT=1;
+		final int EMP_RATE_PER_HR=20;
+		final int IS_FULL_TIME_HRS=8;
 		double empCheck = Math.floor(Math.random() * 10 %2 );
-		if(empCheck == IS_EMP_PRESENT)	
+		if(empCheck == IS_EMP_PRESENT) {	
 			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+			int salary = EMP_RATE_PER_HR * IS_FULL_TIME_HRS;
+			System.out.println("Salary  "+salary);
+		}
+
 
 
 	}
