118cff2bd482b053449ed299542e799c5549b842
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-25 15:11:23 +0530

    welcome file

---
+++ src/com/empwagebuildersolution/EmpWageBuilderSolution.java
@@ -0,0 +1,10 @@
+package com.empwagebuildersolution;
+
+public class EmpWageBuilderSolution {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to empwage calculation");
+
+	}
+
+}
