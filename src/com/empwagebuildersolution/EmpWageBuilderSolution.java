b9b4d44a74b32affb7a7fcf3cd5dc2ec7604bc4f
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-26 21:56:12 +0530

    emp wage for multiple companies

--- src/com/empwagebuildersolution/EmpWageBuilderSolution.java
+++ src/com/empwagebuildersolution/EmpWageBuilderSolution.java
@@ -1,17 +1,16 @@
 package com.empwagebuildersolution;
 
 public class EmpWageBuilderSolution {
-	public static final int IS_PART_TIME=1;
+	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 	
-	public static void main(String[] args){
-		int empHrs=0; 
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-		while (totalEmpHrs<= MAX_HRS_IN_MONTH  && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+	public static int computeEmpWage(String company,int empRatePerHour,
+										int numOfWorkingDays,int maxHoursPerMonth){
+		//variables
+		int empHrs = 0,totalEmpHrs = 0,totalWorkingDays = 0;
+		//Computation
+		while (totalEmpHrs <= maxHoursPerMonth  && 
+				totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck =(int) Math.floor(Math.random() * 10 ) % 3 ;
 			switch (empCheck) {
@@ -28,7 +27,13 @@ public class EmpWageBuilderSolution {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#:" + totalWorkingDays + " Emp Hr:" +empHrs);
 		}
-		int totalEmpWage = totalEmpHrs *EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage:" + totalEmpWage);
-}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage For Company:" +company+ " is:"+ totalEmpWage);
+		return totalEmpWage;
+		
+	}
+	public static void main(String[] args){
+		computeEmpWage("Dmart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
+	}
 }
